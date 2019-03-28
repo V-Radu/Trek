aa462365de34ddaf88d886548991a6641710aec8
Radu Verdes <vradu777@yahoo.com>
2019-03-28 16:36:49 +0000

    First commit

---
+++ src/backend/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+<form action="./index.html">
+  <button type="submit" class="btn btn-secondary">Back</button>
+</form>
+
+<?php
+  echo "Hello World!";
+?>
+
+</body>
+</html>
